b2a340db3479c240ec7b0e6e1ebc30ced590ef2a
Peter Shi <usstszy@163.com>
2016-10-17 14:21:42 +0800

    添加字形样式

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import A from './typography/a';
+import H1 from './typography/h1';
+import H2 from './typography/h2';
+import H3 from './typography/h3';
+import H4 from './typography/h4';
+import H5 from './typography/h5';
+import H6 from './typography/h6';
+import P from './typography/p';
+
+import Button from './button';
+import Card from './card';
+import Badge from './badge';
+import Note from './note';
+import Content from './content';
+import Container from './container';
+
+export {
+  carbonStyles,
+  colors,
+} from './styles';
+
+export { H1 };
+export { H2 };
+export { H3 };
+export { H4 };
+export { H5 };
+export { H6 };
+export { P };
+export { A };
+export { Button };
+export { Card };
+export { Badge };
+export { Note };
+export { Range };
+export { Content };
+export { Container };
+
+export default {
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  P,
+  A,
+  Button,
+  Card,
+  Badge,
+  Note,
+  Range,
+  Content,
+  Container,
+};
